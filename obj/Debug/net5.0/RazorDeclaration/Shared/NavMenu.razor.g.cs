3958daaaf72cf82aa27cba1c7d5dfeafe2a01b1e
Rebwar Hosein Poori <rebwar.poori@hotmail.com>
2021-02-06 21:16:28 +0330

    clean code

--- obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace TodoClient.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
+#line 1 "c:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
+#line 2 "c:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
+#line 3 "c:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
+#line 4 "c:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
+#line 5 "c:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
+#line 6 "c:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
+#line 7 "c:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
+#line 8 "c:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
+#line 9 "c:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
 using TodoClient;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
+#line 10 "c:\Users\Rebwa\Desktop\Todo Project\TodoClient\_Imports.razor"
 using TodoClient.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using TodoClient.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 24 "C:\Users\Rebwa\Desktop\Todo Project\TodoClient\Shared\NavMenu.razor"
+#line 24 "c:\Users\Rebwa\Desktop\Todo Project\TodoClient\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
