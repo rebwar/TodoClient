051bbd91b8730597940fbfcca787fe379af595bb
Rebwar Hosein Poori <rebwar.poori@hotmail.com>
2021-02-06 21:24:07 +0330

    Merge branch 'master' of https://github.com/rebwar/TodoClient hfghfg

---
+++ Pages/ItemEdit.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using TodoClient.Models;
+using TodoClient.Services;
+
+namespace TodoClient.Pages
+{
+    public partial class ItemEdit
+    {
+        protected string Message=string.Empty;
+
+        protected bool Saved;
+
+        [Inject]
+        public ITodoDataService TodoDataService { get; set; }
+
+        [Parameter]
+        public string Id { get; set; }
+
+        public ItemData Item { get; set; }=new ItemData();
+
+        protected async override Task OnInitializedAsync()
+        {
+            var itemId=Convert.ToInt32(Id);
+            Item=await TodoDataService.GetItemDetail(itemId);
+        }
+
+         
+    }
+}
